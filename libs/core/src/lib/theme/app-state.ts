083c3290b711841b9e24d426fe46be84a89b3ea1
Ayoub Ezzouine <ezzouine.a@gmail.com>
2026-04-10 18:13:23 -0400

    feat(theme): enhance app state management and add mobile menu functionality

--- libs/core/src/lib/theme/app-state.ts
+++ libs/core/src/lib/theme/app-state.ts
@@ -4,16 +4,18 @@ export interface AppState {
     surface?: string;
     darkTheme?: boolean;
     menuActive?: boolean;
+    mobileMenuVisible?: boolean;
     designerKey?: string;
     RTL?: boolean;
 }
 
 export const DEFAULT_APP_STATE: AppState = {
-    preset: 'Nora',
+    preset: 'Lara',
     primary: 'noir',
     surface: null as any,
     darkTheme: false,
     menuActive: true,
+    mobileMenuVisible: false,
     designerKey: 'primeng-designer-theme',
     RTL: false
 };
